c2952d88a039ca23320b622757a7771a5598da31
Menachem Farkash <menachem.farkash1@gmail.com>
2024-07-31 22:55:24 +0300

    Find cube by the initial letter
    finished find cube with initial letter
    currently not validated with bool

--- Typing Game/Assets/_Scripts/TextComponent.cs
+++ Typing Game/Assets/_Scripts/TextComponent.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class TextComponent : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class TextComponent : MonoBehaviour {
+    public char activationLetter;
+    public string word;
+    public char[] brokenWord;
+    public TextMeshPro text;
+    public bool isFocused;
+
+    TypingEngine typingEngine;
+
+
+    private void Start() {
+        text.text = word;
+        brokenWord = word.ToCharArray();
+
+        typingEngine = TypingEngine.instance;
+
+        typingEngine.AddCharToInitialsToAttack(activationLetter);
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Update() {
+        if (isFocused) {
+            text.fontSize = 7;
+        } else {
+            text.fontSize = 5;
+        }
+    }
+
+    public void RemoveLetterFromWord() {
+        brokenWord = brokenWord.ToString().Remove(0, 1).ToCharArray();
+    }
+
+    public void SetWordAsFocused() {
+        print("This cube is now the focus");
+    }
+
+    public void AbbortFromWord() {
+
     }
 }
--- Typing Game/Assets/_Scripts/TypingEngine.cs
+++ Typing Game/Assets/_Scripts/TypingEngine.cs
@@ -1,30 +1,117 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TypingEngine : MonoBehaviour {
+    #region Singleton
+    public static TypingEngine instance;
 
-    public string[] words;
+    private void Awake() {
+        instance = this;
+    }
+
+    #endregion
+
+    public List<string> wordToType;
+    public List<char> initialsToAttack;
+
+    private List<string> lettersArray;
+
+    public TextComponent focusedComponent;
+
+
     public int currentLetterIndex;
 
     public string lastLetterTyped;
 
+    public GameObject cubesContainer;
+    public string[] cubes;
+
+
+
     // Start is called before the first frame update
     void Start() {
-        words = new string[] { "a", "b", "c", "d", "e" };
+        wordToType = new List<string> { };
         currentLetterIndex = 0;
+        lettersArray = new List<string>();
+        PopulateLettersArray();
+
+
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.anyKeyDown && currentLetterIndex <= words.Length - 1) {
+        if (Input.anyKeyDown) {
             lastLetterTyped = Input.inputString;
-            TypeLetter(lastLetterTyped);
+            for (int i = 0; i < lettersArray.Count; i++) {
+                if (lastLetterTyped.Equals(lettersArray[i])) {
+
+                    if (CheckIfLetterInInitials(lastLetterTyped.ToCharArray()[0])) {
+
+                        focusedComponent = FindCubeWithInitial(lastLetterTyped.ToCharArray()[0]);
+                        focusedComponent.SetWordAsFocused();
+
+                        SetWordAsWordToType();
+
+                    };
+                    return;
+                }
+            }
+        }
+    }
+
+    private void PopulateLettersArray() {
+        int foreachCounter = 0;
+        foreach (string kcode in Enum.GetNames(typeof(KeyCode))) {
+            foreachCounter++;
+            if (foreachCounter > 122) {
+                return;
+            }
+
+            if (kcode.Length == 1) {
+                lettersArray.Add(kcode);
+                lettersArray.Add(kcode.ToLower());
+            }
         }
     }
 
     public void TypeLetter(string letter) {
-        if (letter == words[currentLetterIndex]) {
-            print(words[currentLetterIndex]);
+        if (letter == wordToType[currentLetterIndex]) {
+            print(wordToType[currentLetterIndex]);
             currentLetterIndex++;
         }
+    }
+
+    public bool CheckIfLetterInInitials(char letter) {
+        for (int i = 0; i < initialsToAttack.Count; i++) {
+            if (initialsToAttack[i] == letter) {
+                print(letter + " Found In Initials");
+                return true;
+            }
+        }
+        print(letter + " Not Found In Initials");
+        return false;
+    }
+
+    public TextComponent FindCubeWithInitial(char initial) {
+        foreach (TextComponent comp in cubesContainer.GetComponentsInChildren<TextComponent>()) {
+            if (comp.activationLetter == initial) {
+                return comp;
+            }
+        }
+
+        return null;
+    }
+
+    public void SetWordAsWordToType() {
+        foreach (char letter in focusedComponent.brokenWord) {
+            wordToType.Add(letter.ToString());
+        }
+    }
+    public void AddCharToInitialsToAttack(char charToAdd) {
+        initialsToAttack.Add(charToAdd);
+    }
+    public void RemoveCharToInitialsToAttack(char charToRemove) {
+        initialsToAttack.Add(charToRemove);
     }
 }
